7a9a203050e543f89a7acdbd0ee0cde23d39adb2
mandy <mandyhale10@gmail.com>
2025-02-12 13:29:08 +1300

    style: fix layout bug and add next steps for timeline

--- src/components/WorkHistory.tsx
+++ src/components/WorkHistory.tsx
@@ -1,23 +1,22 @@
-"use client";
-import { timeline } from "@/constants/timeline";
-import React from "react";
-import { Paragraph } from "./Paragraph";
-import { Heading } from "./Heading";
-import {
-  IconCheck,
-  IconCheckbox,
-  IconCircleCheckFilled,
-} from "@tabler/icons-react";
+'use client'
+import { timeline } from '@/constants/timeline'
+import React from 'react'
+import { Paragraph } from './Paragraph'
+import { Heading } from './Heading'
+import { IconCircleCheckFilled } from '@tabler/icons-react'
 
 export const WorkHistory = () => {
   return (
     <div>
+      {/* TODO add a connecting line between the timeline items */}
       {timeline.map((item, index) => (
         <div
           className="flex md:flex-row flex-col space-y-10 md:space-y-0 space-x-10 my-20 relative"
           key={`timeline-${index}`}
         >
-          <Paragraph className="w-40">{item.date}</Paragraph>
+          <Paragraph className="w-full md:w-32 md:flex-shrink-0 italic">
+            {item.date}
+          </Paragraph>
           <div>
             <Heading
               as="h5"
@@ -28,27 +27,24 @@ export const WorkHistory = () => {
             <Paragraph className="text-base md:text-base lg:text-base font-semibold">
               {item.title}
             </Paragraph>
-            <Paragraph className="text-sm md:text-sm lg:text-sm mb-4">
-              {item.description}
-            </Paragraph>
+            {/* TODO description becomes a stack list */}
+            <Paragraph className="text-sm mb-4">{item.description}</Paragraph>
 
             {item.responsibilities.map((responsibility, index) => (
-              <Step key={responsibility}>{responsibility}</Step>
+              <Step key={index}>{responsibility}</Step>
             ))}
           </div>
         </div>
       ))}
     </div>
-  );
-};
+  )
+}
 
 const Step = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex space-x-1 items-start my-2">
-      <IconCircleCheckFilled className="h-3 w-4 mt-1 text-neutral-300" />
-      <Paragraph className="text-sm md:text-sm lg:text-sm">
-        {children}
-      </Paragraph>
+      <IconCircleCheckFilled className="h-4 w-4 mt-1 text-neutral-300 flex-shrink-0" />
+      <Paragraph className="text-sm">{children}</Paragraph>
     </div>
-  );
-};
+  )
+}
