2a320d99fba36eadfeccf46c87092aaf3504749e
mandy <mandyhale10@gmail.com>
2025-02-12 13:28:03 +1300

    feat: optional icon on badge component

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import { motion } from 'framer-motion'
 import Link from 'next/link'
@@ -5,11 +6,13 @@ import Link from 'next/link'
 export const Badge = ({
   text,
   href,
+  icon,
   ...props
 }: {
   text: string
   href: string
   props?: React.ComponentProps<typeof Link>
+  icon?: React.ReactNode
 }) => {
   return (
     <Link
@@ -22,24 +25,28 @@ export const Badge = ({
       </span>
       <div className="relative flex space-x-2 items-center z-10 rounded-full bg-transparent py-2 px-4 ring-1 ring-white/10 ">
         <span>{text}</span>
-        <svg
-          width="16"
-          height="16"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <motion.path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="1.5"
-            d="M10.75 8.75L14.25 12L10.75 15.25"
-            initial={{ pathLength: 0 }}
-            animate={{ pathLength: 1 }}
-            transition={{ duration: 1 }}
-          ></motion.path>
-        </svg>
+        {icon ? (
+          <span>{icon}</span>
+        ) : (
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <motion.path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1.5"
+              d="M10.75 8.75L14.25 12L10.75 15.25"
+              initial={{ pathLength: 0 }}
+              animate={{ pathLength: 1 }}
+              transition={{ duration: 1 }}
+            ></motion.path>
+          </svg>
+        )}
       </div>
       <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
     </Link>
